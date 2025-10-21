16ff69390de4aafb1974c60f687c32bef550f000
junnn6ix <ngntdluxd@gmail.com>
2025-10-21 10:55:35 +0700

    fix footer & update logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ export const Footer = () => {
   return (
     <footer className="flex items-center justify-center bg-gray-950/50">
       <div className="max-w-5xl flex items-center justify-between mx-auto px-6 md:px-4 h-fit py-6">
-        <div className="flex flex-wrap md:flex-nowrap md:flex-row md:justify-between gap-4 items-start md:items-center">
-          <div className="w-[30%] logo flex items-start flex-col flex-wrap gap-4">
-            <div className="flex items-center gap-2">
+        <div className="flex flex-wrap md:flex-nowrap md:flex-row justify-between gap-2 items-start md:items-center">
+          <div className="w-[25%] md:w-[30%] logo flex items-start flex-col flex-wrap gap-4">
+            <div className="flex items-center gap-2 w-[20%]">
               <img
-                className="w-[28px]"
+                className="w-[24px] md:w-[28px]"
                 src="https://p1-image.cdn-aihelp.net/FileService/UserFile/0/202509/20250916004905658b9663bc5a7.jpg"
                 alt=""
               />
@@ -17,9 +17,9 @@ export const Footer = () => {
               </a>
             </div>
             <div className="flex items-center flex-col gap-3">
-              <p>Sapere Aude!</p>
+              <p className="text-sm md:text-md">Sapere Aude!</p>
 
-              <div className="flex md:hidden items-center flex-wrap gap-3">
+              {/* <div className="hidden md:hidden items-center flex-wrap gap-3">
                 <a href="#">
                   <i className="ri-github-fill text-[16px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
                 </a>
@@ -32,11 +32,11 @@ export const Footer = () => {
                 <a href="#">
                   <i className="ri-mail-line text-[16px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
                 </a>
-              </div>
+              </div> */}
             </div>
           </div>
 
-          <div className="w-[60%] md:w-[40%] flex items-start flex-col gap-4">
+          <div className="w-[40%] md:w-[60%] flex items-start flex-col gap-4">
             <h3 className="font-bold">Lorem Ipsum</h3>
             <p className="font-product text-[12px]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vero
@@ -45,20 +45,20 @@ export const Footer = () => {
             </p>
           </div>
 
-          <div className="hidden md:flex items-center flex-col justify-center gap-4 md:w-[30%]">
+          <div className="flex items-center flex-col gap-4 w-[15%] md:w-[30%]">
             <h3 className="font-bold">Links</h3>
-            <div className="flex items-center flex-wrap gap-3">
+            <div className="flex items-center justify-center flex-wrap gap-3">
               <a href="https://github.com/junnn6ix" target="_blank">
-                <i className="ri-github-fill text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
+                <i className="ri-github-fill text-[20px] md:text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
               </a>
               <a href="https://www.instagram.com/neckjun__" target="_blank">
-                <i className="ri-instagram-line text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
+                <i className="ri-instagram-line text-[20px] md:text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
               </a>
               <a href="https://www.linkedin.com/in/neckjss" target="_blank">
-                <i className="ri-linkedin-line text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
+                <i className="ri-linkedin-line text-[20px] md:text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
               </a>
               <a href="mailto:jjunnnsowden@gmail.com" target="_blank">
-                <i className="ri-mail-line text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
+                <i className="ri-mail-line text-[20px] md:text-[24px] hover:text-purple-700/70 duration-300 transition-all ease-in-out"></i>
               </a>
             </div>
           </div>
